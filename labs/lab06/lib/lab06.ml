1ab70440117ac39896929305d5f33d5a4d8fb075
Anthony Truong <atruong4@bu.edu>
2026-03-02 15:15:46 -0500

    Finished Lab 6

--- labs/lab06/lib/lab06.ml
+++ labs/lab06/lib/lab06.ml
@@ -1,34 +1,46 @@
 
-let map (f : 'a -> 'b) (l : 'a list) : 'a list =
-  ignore (f, l); assert false
+let rec map (f : 'a -> 'b) (l : 'a list) : 'a list =
+  match l with
+  | [] -> []
+  | h :: t ->f h :: map f t
 
-let filter (f : 'a -> bool) (l : 'a list) : 'a list =
-  ignore (f, l); assert false
+let rec filter (f : 'a -> bool) (l : 'a list) : 'a list =
+  match l with
+  | [] -> []
+  | x :: xs when f x -> x :: filter f xs
+  | _ :: xs -> filter f xs
 
-let fold_left
+let rec fold_left
     (f : 'acc -> 'a -> 'acc)
     (base : 'acc)
     (l : 'a list) : 'acc =
-  ignore (f, base, l); assert false
+  match l with
+  | [] -> base
+  | h :: t -> fold_left f (f base h) t
 
-let fold_right
+let rec fold_right
     (f : 'a -> 'acc -> 'acc)
     (l : 'a list)
     (base : 'acc) : 'acc =
-  ignore (f, l, base); assert false
+  match l with
+  | [] -> base
+  | x :: xs -> f x (fold_right f xs base)
 
 module Matrix = struct
   type 'a t = 'a list list
+  let init (dim : int * int) (f: int -> int -> 'a) : 'a t =
+    let (w,h) = dim in
+    List.init h (fun y -> List.init w (fun x -> f x y)) 
 
   let init (dim : int * int) (f : int -> int -> 'a) : 'a t =
     ignore (dim, f); assert false
 
   let map (f : 'a -> 'b) (m : 'a t) : 'b t =
-    ignore (f, m); assert false
+    map (fun row -> map f row) m
 
   let fold_left
       (f : 'acc -> 'a -> 'acc)
       (base : 'acc)
       (m : 'a t) : 'acc =
-    ignore (f, base, m); assert false
+    fold_left (fun acc row -> fold_left f acc row) base m
 end
